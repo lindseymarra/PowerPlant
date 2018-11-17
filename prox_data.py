7650c280987834bf2ec1e426b60947971156b645
Lindsey <marralindseyj@gmail.com>
2018-11-17 22:11:51 +0000

    single sensor prints distance through mux

--- prox_data.py
+++ prox_data.py
@@ -1,4 +1,5 @@
 import VL53L1X
+from time import sleep
 
 tof = VL53L1X.VL53L1X(i2c_bus=1, i2c_address=0x29)
 tof.open() # initialize i2c bus and configure sensor
@@ -6,4 +7,5 @@ tof.start_ranging(1) # start ranging, 1 = short range, 2 = medium, 3 = long
 while True:
     distance_in_mm=tof.get_distance() # get range in mm
     print(distance_in_mm)
+    sleep(0.5)
 tof.stop_ranging() # stop ranging
