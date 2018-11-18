b8dfdcaa239bca5a08a5667095e6292ef04140c9
Lindsey <marralindseyj@gmail.com>
2018-11-18 21:38:18 +0000

    scripts used during the process of mux & sensorhardware bringup

--- multi_prox.py
+++ multi_prox.py
@@ -3,21 +3,28 @@ from time import sleep
 import VL53L1X
 
 
-tof = VL53L1X.VL53L1X(i2c_bus=1, i2c_address=0x29)
+
 with SMBusWrapper(1) as bus:
+
     # enable register on mux to select a sensor
-    bus.write_byte_data(0x70,0,1)
-    bus.write_byte_data(0x70,1,1)
-    print(bus.read_byte_data(0x70,1))
-    if bus.read_byte_data(0x70,1) == 0:
+    bus.write_byte_data(0x70,1,2)
+
+##    bus.write_byte_data(0x70,1,2)
+
+    if bus.read_byte_data(0x70,2) == 0:
         exit(1)
+
     # configure the selected sensor
+    tof = VL53L1X.VL53L1X(i2c_bus=1, i2c_address=0x29)
     tof.open() # initialize i2c bus and configure sensor
     tof.start_ranging(1) # start ranging, 1 = short range, 2 = medium, 3 = long
+    sleep(0.3)
+    distance_in_mm=tof.get_distance() # get range in mm
+    print(distance_in_mm)
     # read from sensor   
     while 1:
-        distance_in_mm=tof.get_distance() # get range in mm
-        print(distance_in_mm)
+        #distance_in_mm=tof.get_distance() # get range in mm
+        #print(distance_in_mm)
 ##        data = bus.read_byte(0x29,0)
 ##        print(data)
 ##        data = bus.read_byte(0x29,1)
